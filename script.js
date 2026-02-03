b533545d669e10ed9448d9092dd0db3e2217a16a
Trev <skct245@gmail.com>
2026-02-03 19:37:25 +0700

    Add getComputerChoice() function with if and switch

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 console.log("Hello World");
+
+
+function getComputerChoice() {
+    //Generate a random number that's greater than or equal to 0 and less than 1
+    //Multiply that number by 3, round it down to integer, and store it in a variable
+    //If the integer is 0, 1, and 2, return "rock", "paper", "scissors" respectively
+    let computerChoice = Math.floor(Math.random() * 3);
+
+    
+    if (computerChoice === 0) {
+        return "rock";
+    } else if (computerChoice === 1) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+   
+    //OR
+
+    /*
+    switch (computerChoice) {
+        case 0:
+            return "rock";
+        case 1:
+            return "paper";
+        default:
+            return "scissors";
+    }
+    */
+}
