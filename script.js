acf351a484b10d36fa8edd7b197ef4eb9e6b96cc
Trev <skct245@gmail.com>
2026-03-05 15:01:33 +0700

    Deleted all working comments

--- script.js
+++ script.js
@@ -18,69 +18,11 @@ function getComputerChoice() {
     }
 }
 
-/*
-console.log(getComputerChoice());
-console.log(getComputerChoice());
-console.log(getComputerChoice());
-console.log(getComputerChoice());
-console.log(getComputerChoice());
-*/
-
 function getHumanChoice() {
     let humanChoice = prompt("What's your choice?",'').toLowerCase();
     humanChoice = humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1);
     return humanChoice;
 }
-
-//console.log(getHumanChoice());
-
-//Initialize counter variables - humanScore and computerScore - and set both to 0
-/*
-let humanScore = 0;
-let computerScore = 0;
-
-
-console.log(humanScore);
-console.log(computerScore);
-
-console.log(typeof humanScore);
-console.log(typeof computerScore);
-*/
-
-//create playRound function
-/*
-function playRound(humanChoice, computerChoice) {
-    if (humanChoice === computerChoice) {
-        return "It's a tie!";
-    } else if ((humanChoice === "rock" && computerChoice === "scissors") || 
-    (humanChoice === "scissors" && computerChoice === "paper") || 
-    (humanChoice === "paper" && computerChoice === "rock")) {
-        humanScore += 1;
-        return `You win! ${humanChoice} beats ${computerChoice}!`;
-    } else {
-        computerScore += 1;
-        return `You lose! ${computerChoice} beats ${humanChoice}`;
-    }
-}
-*/
-
-/*
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-console.log(humanSelection);
-console.log(computerSelection);
-*/
-
-//console.log(playRound(humanSelection, computerSelection));
-
-/*
-check if function works using hardcoded choices
-playRound("Rock","Scissors");
-playRound("Rock","Paper");
-playRound("Rock","Rock");
-console.log(humanScore);
-console.log(computerScore);
-*/
 
 //create playGame function; move playRound and score variables in it;
 function playGame() {
